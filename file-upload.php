39a3d201e01d5c14a5da9fb77478bff44f782edb
ronphobos <sidibearouna71@gmail.com>
2022-05-16 10:53:43 +0000

    4th commit

---
+++ file-upload.php
@@ -0,0 +1,61 @@
+<?php 
+    // Database
+    include 'config/database.php'; 
+    if(isset($_POST['submit'])){
+        
+        $uploadsDir = "uploads/";
+        $allowedFileType = array('jpg','png','jpeg');
+        
+        // Velidate if files exist
+        if (!empty(array_filter($_FILES['fileUpload']['name']))) {
+            
+            // Loop through file items
+            foreach($_FILES['fileUpload']['name'] as $id=>$val){
+                // Get files upload path
+                $fileName        = $_FILES['fileUpload']['name'][$id];
+                $tempLocation    = $_FILES['fileUpload']['tmp_name'][$id];
+                $targetFilePath  = $uploadsDir . $fileName;
+                $fileType        = strtolower(pathinfo($targetFilePath, PATHINFO_EXTENSION));
+                $uploadDate      = date('Y-m-d H:i:s');
+                $uploadOk = 1;
+                if(in_array($fileType, $allowedFileType)){
+                        if(move_uploaded_file($tempLocation, $targetFilePath)){
+                            $sqlVal = "('".$fileName."', '".$uploadDate."')";
+                        } else {
+                            $response = array(
+                                "status" => "alert-danger",
+                                "message" => "Erreur."
+                            );
+                        }
+                    
+                } else {
+                    $response = array(
+                        "status" => "alert-danger",
+                        "message" => "Only .jpg, .jpeg and .png file formats allowed."
+                    );
+                }
+                // Add into MySQL database
+                if(!empty($sqlVal)) {
+                    $insert = $conn->query("INSERT INTO user (images, date_time) VALUES $sqlVal");
+                    if($insert) {
+                        $response = array(
+                            "status" => "alert-success",
+                            "message" => "Fichiers chargés avec succès."
+                        );
+                    } else {
+                        $response = array(
+                            "status" => "alert-danger",
+                            "message" => "Erreur."
+                        );
+                    }
+                }
+            }
+        } else {
+            // Error
+            $response = array(
+                "status" => "alert-danger",
+                "message" => "Veuillez choisir des fichiers."
+            );
+        }
+    } 
+?>
